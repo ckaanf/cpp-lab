963019bbbef6a7b93d0902f2eb5503af953629d9
Kim Jin Soo <ckaanf@accurasoft.co.kr>
2025-04-24 13:18:30 +0900

    learn : cpp output

---
+++ practice-basic/helloworld.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   // Print Text
+   cout << "single line";
+   cout << "Hello, World!";
+   
+   cout << "multi line";
+   cout << "Hello, World!";
+   cout << "I am learning C++!\n";
+
+   // Print Numbers
+   cout << 3;
+   cout << 3 + 3;
+   cout << 2 * 5 << endl;
+
+   // New Line
+   cout << "Hello, World ! \n";
+   cout << "I am learning C++!\n";
+
+   cout << "Hello, World!" << "\n";
+   cout << "I am learning C++!\n";
+
+   cout << "Hello, World!" << "\n\n";
+   cout << "I am learning C++!\n";
+
+   cout << "Hello, World!" << endl;
+   cout << "I am learning C++!\n";
+
+   return 0;
+}
